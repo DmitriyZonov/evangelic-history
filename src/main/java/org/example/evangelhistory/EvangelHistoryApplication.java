be0176e0a3ddc094f8efd101322987365d378895
DmitriyZonov <beeline343@hotmail.com>
2025-10-06 18:00:09 +0200

    start

---
+++ src/main/java/org/example/evangelhistory/EvangelHistoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.evangelhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvangelHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvangelHistoryApplication.class, args);
+    }
+
+}
