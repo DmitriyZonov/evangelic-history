0688c139b0b927b4e1391a4c06effac807cc6640
Dmitriy Zonov <beeline343@gmail.com>
2026-02-17 21:26:15 +0200

    application update

--- src/main/java/org/example/evangelhistory/configs/WebConfig.java
+++ src/main/java/org/example/evangelhistory/configs/WebConfig.java
@@ -2,8 +2,11 @@ package org.example.evangelhistory.configs;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ContentVersionStrategy;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(@NotNull ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        VersionResourceResolver versionResolver = new VersionResourceResolver()
+                .addVersionStrategy(new ContentVersionStrategy(), "/**");
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true) // Включаем цепочку ресурсов
+                .addResolver(versionResolver);
     }
 }
