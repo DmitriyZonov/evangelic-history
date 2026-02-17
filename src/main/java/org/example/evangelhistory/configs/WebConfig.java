5a1a89d6fbe665eeeb42bc512b73df0a332d46b1
Dmitriy Zonov <beeline343@gmail.com>
2026-02-17 21:39:08 +0200

    web config update

--- src/main/java/org/example/evangelhistory/configs/WebConfig.java
+++ src/main/java/org/example/evangelhistory/configs/WebConfig.java
@@ -16,19 +16,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
     }
-
-    @Override
-    public void addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry registry) {
-        org.springframework.web.servlet.resource.VersionResourceResolver versionResolver =
-                new org.springframework.web.servlet.resource.VersionResourceResolver()
-                        .addVersionStrategy(new org.springframework.web.servlet.resource.ContentVersionStrategy(), "/**");
-
-        // Явно прописываем папки, чтобы Spring не путался
-        registry.addResourceHandler("/css/**", "/js/**", "/img/**", "/uploads/**")
-                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/", "classpath:/static/img/", "classpath:/static/uploads/")
-                .setCachePeriod(3600) // Кэш на час для надежности
-                .resourceChain(true)
-                .addResolver(versionResolver)
-                .addResolver(new org.springframework.web.servlet.resource.PathResourceResolver());
-    }
 }
