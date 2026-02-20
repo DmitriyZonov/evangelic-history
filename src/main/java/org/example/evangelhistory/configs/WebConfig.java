8fc11946861e29980c516203ba8da5453d1d1da9
Dmitriy Zonov <beeline343@gmail.com>
2026-02-20 18:22:13 +0200

    add photos of event

--- src/main/java/org/example/evangelhistory/configs/WebConfig.java
+++ src/main/java/org/example/evangelhistory/configs/WebConfig.java
@@ -1,11 +1,12 @@
 package org.example.evangelhistory.configs;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.ContentVersionStrategy;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
@@ -15,5 +16,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(@NotNull ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600) // Опционально: кэш на 1 час для браузера
+                .resourceChain(true)
+                .addResolver(new VersionResourceResolver()
+                        .addContentVersionStrategy("/**"));
+    }
+    @Bean
+    public static ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+        return new ResourceUrlEncodingFilter();
     }
 }
